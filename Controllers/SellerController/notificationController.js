a9794fc423f8c456ef52428cf4bc824859f156cd
Naman Prakash Agrawal <traineenaman.techgropse@gmail.com>
2022-07-25 17:04:53 +0530

    done

--- Controllers/SellerController/notificationController.js
+++ Controllers/SellerController/notificationController.js
@@ -1,10 +1,10 @@
 const { success, error } = require("../../service_response/adminApiResponse");
-const admin = require("firebase-admin");
-const serviceAccount = require("../../config/firebase.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://freshtrader-78321-default-rtdb.firebaseio.com",
-});
+// const admin = require("firebase-admin");
+// const serviceAccount = require("../../config/firebase.json");
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://freshtrader-78321-default-rtdb.firebaseio.com",
+// });
 exports.sendNotification = async (req, res, next) => {
   try {
     const notification_options = {
@@ -22,20 +22,20 @@ exports.sendNotification = async (req, res, next) => {
       },
       data: {},
     };
-    admin
-      .messaging()
-      .sendToDevice(
-        "cCjfaJCYPtY8-Jb5LuYOOA:APA91bEkkIwEpC0ExSuN3Jx1-Bp2CDXBQ_7QAO4t6crH83n2Om8F4B5hPViDsl9NPYKeYW8Z4WSNhzCruuNlhU-vANm2e8pKpYjjo7ukDx8Ck_2ssEl7Np02r-B8wFOEpK6HrooV3u2X",
-        payload,
-        notification_options
-      )
-      .then((response) => {
-        console.log(response.results);
-        return;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    // admin
+    //   .messaging()
+    //   .sendToDevice(
+    //     "cCjfaJCYPtY8-Jb5LuYOOA:APA91bEkkIwEpC0ExSuN3Jx1-Bp2CDXBQ_7QAO4t6crH83n2Om8F4B5hPViDsl9NPYKeYW8Z4WSNhzCruuNlhU-vANm2e8pKpYjjo7ukDx8Ck_2ssEl7Np02r-B8wFOEpK6HrooV3u2X",
+    //     payload,
+    //     notification_options
+    //   )
+    //   .then((response) => {
+    //     console.log(response.results);
+    //     return;
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
     return;
   } catch (err) {
     console.log(err);
