364453e04e89ae3bf453050505a1bad41515e552
Naman Prakash Agrawal <traineenaman.techgropse@gmail.com>
2022-08-02 19:03:14 +0530

    done

--- Controllers/SellerController/transactionController.js
+++ Controllers/SellerController/transactionController.js
@@ -504,10 +504,10 @@ exports.downloadTransactionCSV = async (req, res, next) => {
         POPostalCode: transaction.buyer.postal_code,
         POCountry: transaction.buyer.country,
         Time: moment(transaction.createdAt).format("hh:mm A"),
-        Date: moment(transaction.createdAt).format("dd/mm/yyyy"),
+        Date: moment(transaction.createdAt).format("DD-MM-YYYY"),
         DueDate:
           transaction.status !== "PAID"
-            ? moment(due_date).format("dd/mm/yyyy")
+            ? moment(due_date).format("DD-MM-YYYY")
             : "",
         InventoryItemCode: inventory_code.join(", "),
         Description: transaction.delivery_note,
